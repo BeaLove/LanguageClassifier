408cb9a5b3cce1744ae2c37fc50c520fb744680d
BeaLove <beatriceelovey@gmail.com>
2021-07-02 12:03:41 +0200

    argument parser

--- LangClass/train.py
+++ LangClass/train.py
@@ -1,5 +1,5 @@
 import torch.nn
-
+import argparse
 from wav2vecclassifier import LanguageClassifier
 from dataloader import SentenceData
 from torch.utils.data import DataLoader, Subset
@@ -12,23 +12,23 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 class Trainer():
-    def __init__(self, data_dir):
+    def __init__(self, data_dir, log_dir, patience, checkpoints_dir):
 
         self.model = LanguageClassifier()
-        self.optim = torch.optim.SGD(self.model.parameters(), lr=1e-2)
+        self.optim = torch.optim.Adam(self.model.parameters())
         self.dataset = SentenceData(data_dir)
         indices = torch.randperm(len(self.dataset))
         val_split = int((len(indices)*0.05))
         self.val_set = Subset(self.dataset, indices=indices[:val_split])
-        self.trainset = Subset(self.dataset, indices=indices[500:]) ##TODO change when doing full run
+        self.trainset = Subset(self.dataset, indices=indices[2500:]) ##TODO change when doing full run!
         self.train_loader = DataLoader(self.trainset, shuffle=True, num_workers=0, batch_size=1)
         self.val_loader = DataLoader(self.val_set, shuffle=True, num_workers=0, batch_size=1)
         self.loss_criterion = torch.nn.CrossEntropyLoss()
-        self.tensorboard_writer = torch.utils.tensorboard.SummaryWriter(log_dir='logs')
+        self.tensorboard_writer = torch.utils.tensorboard.SummaryWriter(log_dir=log_dir)
         self.global_loss = 1000
-        self.patience = 5
-        os.makedirs('checkpoints', exist_ok=True)
-        self.checkpt_dir = 'checkpoints'
+        self.patience = patience
+        os.makedirs(checkpoints_dir, exist_ok=True)
+        self.checkpt_dir = checkpoints_dir
         self.best_model = 0
         if torch.cuda.is_available():
             self.device = 'cuda:0'
@@ -67,8 +67,11 @@ class Trainer():
         with torch.no_grad():
             for step, sample in enumerate(batch_i):
                 x, y = sample
+                x = x.to(self.device)
+                y = y.to(self.device)
+                x = x[0, :, :]
                 output = self.model.forward(x)
-                sum_loss += self.loss(output, y)
+                sum_loss += self.loss_criterion(output, y)
             loss = sum_loss/len(self.val_set)
         self.tensorboard_writer.add_scalar(tag="val loss", scalar_value=loss, global_step=epoch)
         return loss
@@ -79,7 +82,7 @@ class Trainer():
             self.train_epoch(epoch)
             val_loss = self.validate(epoch)
             chkpt_name = 'wav2vec_finetune_checkpoint_epoch{}.pt'.format(epoch)
-            self.model.save(os.path.join(self.checkpt_dir, chkpt_name))
+            torch.save(self.model, os.path.join(self.checkpt_dir, chkpt_name))
             if self.early_stop_callback(val_loss, epoch):
                 print("Validation loss did not improve for {} epochs, stopping training!")
 
@@ -87,12 +90,31 @@ class Trainer():
     def early_stop_callback(self, loss, epoch):
         if loss > self.global_loss:
             self.patience -= 1
+            print("val loss did not improve, decreasing patience to: {}".format(self.patience))
         else:
             self.best_model = epoch
+            print("val loss improved!")
         if self.patience == 0:
             return True
         return False
 
-trainer = Trainer(data_dir='data/train')
-trainer.train(epochs=50)
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser(description="Train a Language Classifier on Wav2Vec embeddings")
+    parser.add_argument('--dataset_dir', dest='dataset_dir', help='dataset directory', type=str)
+    parser.add_argument('--checkpoints_dir', dest='ckpt_dir', default='checkpoints', type=str)
+    parser.add_argument('--log_dir', dest='log_dir', default='logs', type=str)
+    parser.add_argument('--epochs', dest='epochs', help='training epochs', type=int)
+    parser.add_argument('--patience', dest='patience', help='early stop patience', default=5, type=int)
+    '''currently not used'''
+    parser.add_argument('-learning_rate', dest='lr', default=1e-2, type=float)
+    parser.add_argument('--warm_up', dest='warm_up', default=0, type=int, help='number of warmup steps for optimizer')
+    parser.add_argument('--decay_steps', dest='decay', default=0, type=int, help='number of steps to decay learning rate' )
+    args = parser.parse_args()
+    print(args)
+    return args
+
+if __name__ == "__main__":
+    args = parse_args()
+    trainer = Trainer(data_dir=args.dataset_dir, checkpoints_dir=args.checkpoints_dir, log_dir=args.log_dir, patience=args.patience)
+    trainer.train(epochs=args.epochs)
 
--- LangClass/wav2vecclassifier.py
+++ LangClass/wav2vecclassifier.py
@@ -7,19 +7,21 @@ class LanguageClassifier(nn.Module):
     def __init__(self, out_classes=3, pool_kernel=5):
         '''instantiates a 3-language classifier for Swedish, English and Arabic'''
         super().__init__()
-        config = Wav2Vec2Config(return_dict=False)
-
-        self.encoder = Wav2Vec2Model(config).from_pretrained('facebook/wav2vec2-large-xlsr-53')
-
+        self.encoder = Wav2Vec2Model.from_pretrained('facebook/wav2vec2-large-xlsr-53')
+        self.freeze_pretrained(self.encoder)
         self.avg_pooling = nn.AvgPool2d(kernel_size=pool_kernel)
-        self.FC = nn.Linear(5916, out_classes)
-        self.out = nn.Softmax()
+        self.fc = nn.Linear(9996, out_classes)
+
 
     def forward(self, X):
+
         wav2vecout = self.encoder(X)
         context = wav2vecout.last_hidden_state
         pooled = self.avg_pooling(context)
-        vector = torch.flatten(pooled)
-        linear = self.FC(vector)
-        return self.out(linear)
+        vector = pooled.reshape(1,-1)
+        linear = self.fc(vector)
+        return linear
 
+    def freeze_pretrained(self, layer):
+        for param in layer.parameters():
+            param.requires_grad = False

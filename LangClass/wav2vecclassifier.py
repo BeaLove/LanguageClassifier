c8062e2ed1bf36f7c6713cc952db4c1fbf4b467c
BeaLove <beatriceelovey@gmail.com>
2021-07-09 16:01:32 +0200

    diff model

--- LangClass/wav2vecclassifier.py
+++ LangClass/wav2vecclassifier.py
@@ -1,7 +1,7 @@
 
 import torch.nn as nn
 import torch
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Model, Wav2Vec2Processor
 
 
 class LanguageClassifier(nn.Module):
@@ -9,16 +9,17 @@ class LanguageClassifier(nn.Module):
         '''instantiates a 3-language classifier for Swedish, English and Arabic'''
         super().__init__()
         #self.processor = Wav2Vec2Processor.from_pretrained('facebook/wav2vec2-large-xlsr-53')
-        self.encoder = Wav2Vec2ForCTC.from_pretrained('facebook/wav2vec2-large-xlsr-53')
+        #self.encoder = Wav2Vec2ForCTC.from_pretrained('facebook/wav2vec2-large-xlsr-53')
+        self.encoder = Wav2Vec2Model.from_pretrained('facebook/wav2vec2-large-xlsr-53')
         self.freeze_pretrained()
         #self.avg_pooling = nn.AvgPool2d(kernel_size=pool_kernel)
-        self.fc = nn.Linear(32, out_classes)
+        self.fc = nn.Linear(1024, out_classes)
 
 
 
     def forward(self, X):
         wav2vecout = self.encoder(X)
-        context = wav2vecout.logits
+        context = wav2vecout.last_hidden_state
         pooled = torch.mean(context, dim=1)
         #vector = pooled.reshape(pooled.shape[0],pooled.shape[1]*pooled.shape[2])
         linear = self.fc(pooled)
@@ -28,7 +29,6 @@ class LanguageClassifier(nn.Module):
         '''freeze layers in pretrained model'''
         for param in self.encoder.parameters():
             param.requires_grad = False
-        self.frozen = True
         self.frozen = True
 
     def unfreeze_pretrained(self):
